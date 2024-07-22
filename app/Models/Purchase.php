91c5e48b8e6909d1908b03ff2b210193da8aeae4
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-07-22 13:08:32 +0800

    item_puurchase seeder

--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Customer;
+use App\Models\Item;
 
 class Purchase extends Model
 {
@@ -14,10 +15,15 @@ class Purchase extends Model
         'customer_id',
         'status',
     ];
-    
+
     public function customer()
     {
         return $this->belongsTo(Customer::class);
     }
 
+    public function items()
+    {
+        return $this->belongsToMany(Item::class)
+        ->withPivot('quantity');
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Purchase;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,12 +22,17 @@ class DatabaseSeeder extends Seeder
         ]);
 
         \App\Models\Customer::factory(1000)->create();
-        \App\Models\Purchase::factory(100)->create();
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $items = \App\Models\Item::all();
+        Purchase::factory(100)->create()
+        ->each(function(Purchase $purchase) use ($items){
+            $purchase->items()->attach(
+                $items->random(rand(1, 3))
+                ->pluck('id')
+                ->toArray(),
+                // 1~3個のitemをpurchaseにランダムに紐付け
+                ['quantity' => rand(1, 5)]
+            );
+        });
     }
 }
