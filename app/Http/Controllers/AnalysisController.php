cc02b8a3dc78304bd4294b4cd92f5c004b8f4f25
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-07-31 02:31:18 +0800

    Analysisページ作成、Ajax通信

---
+++ app/Http/Controllers/AnalysisController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\Order;
+
+class AnalysisController extends Controller
+{
+    public function index()
+    {
+        $startDate = '2024-07-30';
+        $endDate = '2024-07-31';
+
+        $period = Order::betweenDate($startDate, $endDate)
+        ->groupBy('id')
+        ->selectRaw('id, sum(subtotal) as total,
+        customer_name, status, created_at')
+        ->orderBy('created_at')
+        ->paginate(50);
+
+        return Inertia::render('Analysis');
+    }
+}
---
+++ app/Http/Controllers/Api/AnalysisController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\Order;
+
+
+class AnalysisController extends Controller
+{
+    public function index(Request $request)
+    {
+        // Ajax通信なのでJsonで返却する必要がある
+        return response()->json([
+            'data' => $request->startDate
+        ], Response::HTTP_OK 
+        );
+    }
+}
