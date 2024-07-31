5dd7a6e1a3bb7d677b82924f5c2decf333ebc5ae
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-08-01 01:20:22 +0800

    日別クエリ作成

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -5,20 +5,29 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\Order;
+use Illuminate\Support\Facades\DB;
 
 class AnalysisController extends Controller
 {
     public function index()
     {
-        $startDate = '2024-07-30';
+        // 期間指定
+        $startDate = '2024-06-30';
         $endDate = '2024-07-31';
 
-        $period = Order::betweenDate($startDate, $endDate)
+        // 日別
+        $subQuery = Order::betweenDate($startDate, $endDate)
+        ->where('status', true)
         ->groupBy('id')
-        ->selectRaw('id, sum(subtotal) as total,
-        customer_name, status, created_at')
-        ->orderBy('created_at')
-        ->paginate(50);
+        ->selectRaw('id, SUM(subtotal) as totalPerPurchase,
+        DATE_FORMAT(created_at, "%Y%m%d") as date');
+
+        $data = DB::table($subQuery)
+        ->groupBy('date')
+        ->selectRaw('date, sum(totalPerPurchase) as total')
+        ->get();
+
+        //dd($data);
 
         return Inertia::render('Analysis');
     }
