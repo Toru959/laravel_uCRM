6cabbcb1bdc0fc520a59dbdb04a13b26a6e742f0
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-08-02 19:15:23 +0800

    デシル分析

--- resources/js/Pages/Analysis.vue
+++ resources/js/Pages/Analysis.vue
@@ -54,6 +54,12 @@ const getData = async () => {
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <form @submit.prevent="getData">
+                        分析方法<br>
+                        <input type="radio" v-model="form.type" value="perDay" checked><span class="mr-2">日別</span>
+                        <input type="radio" v-model="form.type" value="perMonth"><span class="mr-2">月別</span>
+                        <input type="radio" v-model="form.type" value="perYear"><span class="mr-2">年別</span>
+                        <br>
+
                         From: <input type="date" name="startDate" v-model="form.startDate">
                         To: <input type="date" name="endDate" v-model="form.endDate">
                         <button type="submit" class="ml-4 mt-4 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">分析する</button>
